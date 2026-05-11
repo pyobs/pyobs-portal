5ad9d2f622287cd3674925369be2d95fd03ffb56
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-05-11 14:29:38 +0200

    serializing target

--- pyobs_robotic_backend/api/serializers.py
+++ pyobs_robotic_backend/api/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from .models import Task, Observation, Merit, Constraint, Project
+from .models import Task, Observation, Merit, Constraint, Project, Target
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -57,9 +57,34 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class TargetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Target
+        fields = ["name", "type", "coords"]
+
+    def to_internal_value(self, data):
+        klass = data.pop("class")
+        if "SiderealTarget" in klass:
+            data["type"] = "sidereal"
+            data["coords"] = {"ra": data.pop("ra"), "dec": data.pop("dec")}
+        return data
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        typ = data.pop("type")
+        if typ == "sidereal":
+            data["class"] = "pyobs.robotic.scheduler.targets.SiderealTarget"
+            coords = data.pop("coords")
+            data["ra"] = coords["ra"]
+            data["dec"] = coords["dec"]
+
+        return data
+
+
 class TaskSerializer(serializers.ModelSerializer):
     constraints = ConstraintSerializer(many=True)
     merits = MeritSerializer(many=True)
+    target = TargetSerializer()
     project = serializers.SlugRelatedField(read_only=True, slug_field="code")
 
     class Meta:
@@ -87,14 +112,51 @@ class TaskSerializer(serializers.ModelSerializer):
         except Project.DoesNotExist:
             raise ValidationError("Project not found")
         task = Task.objects.create(project=project, **validated_data)
+        self._create_constraints(task, constraints_data)
+        self._create_merits(task, merits_data)
+        if target_data:
+            pass
+        return task
+
+    def update(self, task: Task, validated_data):
+        merits_data = validated_data.pop("merits")
+        constraints_data = validated_data.pop("constraints")
+        target_data = validated_data.pop("target", None)
+        project_code = validated_data.pop("project")
+        try:
+            validated_data["project"] = Project.objects.get(code=project_code)
+        except Project.DoesNotExist:
+            raise ValidationError("Project not found")
+        super().update(task, validated_data)
+
+        task.constraints.all().delete()
+        self._create_constraints(task, constraints_data)
+        task.merits.all().delete()
+        self._create_merits(task, merits_data)
+        try:
+            task.target.delete()
+        except Task.target.RelatedObjectDoesNotExist:
+            pass
+        self._create_target(task, target_data)
+
+        return task
+
+    @staticmethod
+    def _create_merits(task: Task, merits_data):
         for merit in merits_data:
             Merit.objects.create(
                 task=task, **MeritSerializer().to_internal_value(merit)
             )
+
+    @staticmethod
+    def _create_constraints(task: Task, constraints_data):
         for constraint in constraints_data:
             Constraint.objects.create(
                 task=task, **ConstraintSerializer().to_internal_value(constraint)
             )
-        if target_data:
-            pass
-        return task
+
+    @staticmethod
+    def _create_target(task: Task, target_data):
+        Target.objects.create(
+            task=task, **TargetSerializer().to_internal_value(target_data)
+        )
