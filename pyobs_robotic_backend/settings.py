4d36129b200cb66957b0002dcab1c9ca5a41c7b0
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-05-18 10:57:16 +0200

    STATIC_ROOT from env

--- pyobs_robotic_backend/settings.py
+++ pyobs_robotic_backend/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
 
 try:
     from .local_settings import *
